0f0ade14e52468202c4cd51629c1c71104c78cc4
Hieu Nguyen <hieu.nguyen@simpo.vn>
2023-11-20 14:11:47 +0700

    Add BackStage template

---
+++ backstage-plugins/aws-apps/src/index.ts
@@ -0,0 +1,29 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export {
+  AwsEnvironmentPage,
+  AwsEnvironmentProviderPage,
+  AwsComponentPage,
+  AwsAppPage,
+  opaPlugin,
+  EntityAnnotationTypeTable,
+  EntityAppStateCard,
+  EntityAppPromoCard,
+  EntityAppStateCardCloudFormation,
+  EntityLabelTable,
+  EntityGeneralInfoCard,
+  EntityDeleteAppCard,
+  EntityDeleteProviderCard,
+  EntityDeleteEnvironmentCard,
+  EntityResourceBindingCard,
+  AppCatalogPage,
+  EntityCloudwatchLogsTable,
+  EntityInfrastructureInfoCard,
+  EntityProviderInfoCard,
+  EntityEnvironmentInfoCard,
+  EntityAppConfigCard,
+  EntityAuditTable,
+  EntityEnvironmentSelector,
+  EntityAwsEnvironmentProviderSelectorCard
+} from './plugin';
